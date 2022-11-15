e8f2c512dc4a4d8b6a71245faf9c0049926473c0
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-15 18:39:20 -0300

    fix: Correção do middleware de error

--- src/middleware/middleware.error.ts
+++ src/middleware/middleware.error.ts
@@ -1,22 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import IErrorResponse from '../interfaces/IErrorResponse';
 
-class Error {
-  static error(
-    err: IErrorResponse,
-    _req: Request,
-    res: Response,
-    _next: NextFunction
-  ): Response {
-    const { error } = err;
-    if (error) {
-      return res.status(error.code).json({ message: error.message });
-    }
-    return res.status(500).json({
-      message:
-        'Tivemos um erro Interno, aguarde um tempo e recarregue a página!',
-    });
+const errorResponse = (
+  err: IErrorResponse,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+): Response => {
+  const { error } = err;
+  if (error) {
+    return res.status(error.code).json({ message: error.message });
   }
-}
+  return res.status(500).json({
+    message: 'Tivemos um erro Interno, aguarde um tempo e recarregue a página!',
+  });
+};
 
-export default Error.error;
+export default errorResponse;
