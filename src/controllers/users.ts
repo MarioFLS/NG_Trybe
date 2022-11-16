0418617a88161dc24c4bfab30337dc470efc56a2
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-16 12:06:24 -0300

    feat: Rota de login

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import IErrorResponse from '../interfaces/IErrorResponse';
 import IUser from '../interfaces/IUser';
-import { createUser } from '../services/service.users';
+import { createUser, loginUser } from '../services/service.users';
 
 const create = async (
   req: Request,
@@ -17,4 +17,9 @@ const create = async (
   return res.status(StatusCodes.CREATED).json({ id, username });
 };
 
-export { create };
+const login = (req: Request, res: Response): Response => {
+  const token = loginUser(req.body);
+  return res.status(StatusCodes.CREATED).json({ token });
+};
+
+export { create, login };
--- src/routes/route.users.ts
+++ src/routes/route.users.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { create } from '../controllers/users';
+import { create, login } from '../controllers/users';
+import validateLogin from '../middleware/validateLogin';
 
 const userRouter = express.Router();
 
 userRouter.post('/', create);
+userRouter.post('/login', validateLogin, login);
 
 export default userRouter;
