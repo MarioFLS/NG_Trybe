7b314acbae3b41ce2e633bb128a2af28bf4db876
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-16 15:48:55 -0300

    feat: procurar balance por usuário

--- src/helpers/searchDatabase.ts
+++ src/helpers/searchDatabase.ts
@@ -1,5 +1,10 @@
+import Accounts from '../database/models/accounts';
 import Users from '../database/models/users';
 
 const findByUsername = async (username: string) => Users.findOne({ where: { username } });
 
+const findByAccount = async (accountId: number) => Accounts
+  .findOne({ where: { id: accountId } });
+
 export default findByUsername;
+export { findByAccount };
