5b11cc6c8aff8f6c76bf4ffb305817b46ecb99db
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-16 12:03:15 -0300

    feat: comparação de hash

--- src/helpers/hashPassword.ts
+++ src/helpers/hashPassword.ts
@@ -1,8 +1,22 @@
 import bcrypt from 'bcryptjs';
+import ILogin from '../interfaces/ILogin';
+import IUser from '../interfaces/IUser';
+import findByUsername from './searchDatabase'
 
-const hashPasswordDB = (password: string) => {
-  const salt = bcrypt.genSaltSync(10);
-  return bcrypt.hashSync(password.trim(), salt);
+const salt = bcrypt.genSaltSync(10);
+
+const hashPasswordDB = (password: string) => bcrypt.hashSync(password.trim(), salt);
+
+const compareHashPassword = async (data: ILogin) => {
+  const { username, password } = data;
+
+  const findUser = await findByUsername(username)
+
+  if (!findUser) return null;
+
+  const user: IUser = findUser?.toJSON();
+  return bcrypt.compareSync(password, user.password);
 };
 
 export default hashPasswordDB;
+export { compareHashPassword }
