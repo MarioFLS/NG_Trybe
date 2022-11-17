a176ede57bde7bf0e8c497a71953550578ce2662
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-17 17:05:27 -0300

    feat: validação de criação do usuário

---
+++ src/middleware/validate.user.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { compareHashPassword } from '../helpers/hashPassword';
+
+const validateUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { username, password } = req.body;
+  const url = req.url === 'login';
+
+  if (!username || !password) {
+    return res.status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'Precisa de um username e um password.' });
+  }
+
+  if (!url) {
+    return next();
+  }
+
+  const hash = await compareHashPassword(req.body)
+  if (!hash) {
+    return res.status(StatusCodes.UNAUTHORIZED)
+      .json({ message: 'Senha ou usuário incorretos.' });
+  }
+  return next();
+};
+
+export default validateUser;
--- src/routes/route.users.ts
+++ src/routes/route.users.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import {
   getBalance, create, login, cashOut, transactions, deposit, withdrawal,
 } from '../controllers/users';
-import validateLogin from '../middleware/validate.login';
+import validateUser from '../middleware/validate.user';
 import validateToken from '../middleware/validate.token';
 
 const userRouter = express.Router();
 
-userRouter.post('/', create);
-userRouter.post('/login', validateLogin, login);
+userRouter.post('/', validateUser, create);
+userRouter.post('/login', validateUser, login);
 userRouter.get('/balance', validateToken, getBalance);
 userRouter.post('/cashOut', validateToken, cashOut);
 userRouter.get('/transactions', validateToken, transactions);
