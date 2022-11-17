7fbde3a712cc76b1b3708ed73c59eb89c02c0150
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-17 20:52:04 -0300

    feat: Criação de db de teste

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -4,9 +4,9 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'password',
-  database: 'NG_trybe',
+  database: process.env.DB_TEST || 'NG_trybe',
   host: 'localhost',
-  port: 5430,
+  port: Number(process.env.DB_PORT_TEST) || 5430,
   dialect: 'postgres',
 };
 
