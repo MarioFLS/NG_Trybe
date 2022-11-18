91530704032484da0d7af6479d5ac96ce1790b3a
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-17 21:24:12 -0300

    feat: criando mock de usuário

---
+++ test/mock/mock.user.ts
@@ -0,0 +1,44 @@
+const userOne = {
+  username: 'Jorge',
+  password: '12345678bC',
+};
+
+const userTwo = {
+  username: 'Marcos',
+  password: '12345678bC',
+};
+
+const userThree = {
+  username: 'Fabio',
+  password: '12345678bC',
+};
+
+const invalidUserName = {
+  username: '',
+  password: '12345678bC',
+};
+
+const invalidUserPassword = {
+  username: 'Gustavo',
+  password: '12345678bd',
+};
+
+const fakeUser = {
+  username: 'Fake',
+  password: '12345678bC',
+};
+
+const failUser = {
+  username: 'Teste',
+  password: '',
+};
+
+export {
+  userOne,
+  userTwo,
+  userThree,
+  invalidUserName,
+  invalidUserPassword,
+  fakeUser,
+  failUser
+};
