339475443a6acbd9e1c61343186e8c56ea48fee3
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-19 20:08:42 -0300

    feat: lid do cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import swaggerConfig from '../docs/swagger'
 import server from './server';
@@ -8,10 +9,11 @@ import errorResponse from './middleware/middleware.error';
 
 const port = process.env.APP_PORT || 3002;
 
+server.use(cors());
 server.use(userRouter);
-
-server.use(errorResponse);
 
 server.all('/docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 server.all('*', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
+
+server.use(errorResponse);
 server.listen(port, () => console.log('Olá, você está na porta', port));
